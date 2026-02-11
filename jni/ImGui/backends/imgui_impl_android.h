08fc26066e051bb1803c3813ec998a27dd056607
CatCrying <88854387+CatCrying@users.noreply.github.com>
2026-02-11 20:06:41 +0700

    Add files via upload

---
+++ jni/ImGui/backends/imgui_impl_android.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <EGL/egl.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+bool ImGui_ImplAndroid_Init(EGLDisplay display, EGLSurface surface);
+void ImGui_ImplAndroid_NewFrame(void);
+void ImGui_ImplAndroid_Shutdown(void);
+bool ImGui_ImplAndroid_HandleInputEvent(void* event);
+#ifdef __cplusplus
+}
+#endif
